36edb9f1c50d68e84e82dc08137fc877bdd47bdf
Md. Nuho-Ul Alam Kiam <mdnuhoulalam@gmail.com>
2021-10-08 20:09:37 +0600

    Create delete.js

---
+++ delete.js
@@ -0,0 +1,23 @@
+document.getElementById('one').addEventListener('click', function (){
+    console.log('div-1')
+}, false)
+
+document.getElementById('two').addEventListener('click', function (){
+    console.log('div-2')
+}, false)
+
+document.getElementById('three').addEventListener('click', function (){
+    console.log('div-3')
+}, false)
+
+document.getElementById('btn-1').addEventListener('click', function (){
+    console.log('btn-1')
+}, false)
+
+document.getElementById('btn-2').addEventListener('click', function (){
+    console.log('btn-2')
+}, false)
+
+document.getElementById('btn-3').addEventListener('click', function (){
+    console.log('btn-3')
+}, false)
